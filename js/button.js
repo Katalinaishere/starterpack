db6a4d1a28e58c0b3da7e4f0c14aebc0f9ce4536
Katalina <47336744+Katalinaishere@users.noreply.github.com>
2023-02-01 19:25:13 +0100

    Update button.js

--- js/button.js
+++ js/button.js
@@ -5,3 +5,10 @@ const addButton = document.getElementById("add");
 addButton.addEventListener("click", function() {
   display.innerHTML += input.value + "<br>";
 });
+function updateTime() {
+  const now = new Date();
+  const timeString = now.toLocaleString();
+  document.querySelector("#current-time").innerHTML = timeString;
+}
+
+setInterval(updateTime, 1000);
